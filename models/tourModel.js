150e187b880e2e35da4d971cedc3daddd91efeee
TalaatEysa <talaateysa@outlook.com>
2024-06-09 03:04:19 +0300

    modifying tours model

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,17 +5,57 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name'],
-        unique: true
+        unique: true,
+        trim: true
 
     },
-    rating: {
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty']
+    },
+    duration: {
+        type: Number,
+        required: [true, 'A tour must have a duration']
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
     },
     price: {
         type: Number,
         required: [true, 'A tour must have a price']
-    }
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a description']
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image']
+    },
+    images: [String],
+    createdBy: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date],
+
+
+    
 
 });
 
