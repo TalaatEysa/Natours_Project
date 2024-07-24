cde28c5d5f3101b8b240a17e07f404bad70ac59f
TalaatEysa <talaateysa@outlook.com>
2024-07-25 00:08:43 +0300

    implementing aggregation middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,77 +2,79 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
+const tourSchema = new mongoose.Schema(
+  {
     // Schema type options
     name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true,
-        trim: true
-
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
     },
     slug: String,
     maxGroupSize: {
-        type: Number,
-        required: [true, 'A tour must have a group size']
+      type: Number,
+      required: [true, 'A tour must have a group size'],
     },
     difficulty: {
-        type: String,
-        required: [true, 'A tour must have a difficulty']
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
     },
     duration: {
-        type: Number,
-        required: [true, 'A tour must have a duration']
+      type: Number,
+      required: [true, 'A tour must have a duration'],
     },
     ratingsAverage: {
-        type: Number,
-        default: 4.5
+      type: Number,
+      default: 4.5,
     },
     ratingsQuantity: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     price: {
-        type: Number,
-        required: [true, 'A tour must have a price']
+      type: Number,
+      required: [true, 'A tour must have a price'],
     },
     priceDiscount: Number,
     summary: {
-        type: String,
-        trim: true,
-        required: [true, 'A tour must have a description']
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
     },
     description: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true,
     },
     imageCover: {
-        type: String,
-        required: [true, 'A tour must have a cover image']
+      type: String,
+      required: [true, 'A tour must have a cover image'],
     },
     images: [String],
     createdBy: {
-        type: Date,
-        default: Date.now(),
-        select: false
+      type: Date,
+      default: Date.now(),
+      select: false,
     },
     startDates: [Date],
     secretTour: {
-        type: Boolean,
-        default: false
-    }
-}, {
-    toJSON:{virtuals: true},
-    toObject:{virtuals: true}
-});
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
 
 tourSchema.virtual('durationWeeks').get(function () {
-    return this.duration / 7;
-})
+  return this.duration / 7;
+});
 //Document Middleware runs before .save() command and .create()
 tourSchema.pre('save', function (next) {
-    this.slug = slugify(this.name, { lower: true });
-    next();
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 // tourSchema.pre('save', function (next) {
@@ -86,15 +88,21 @@ tourSchema.pre('save', function (next) {
 
 //QUERY MIDDLEWARE
 tourSchema.pre(/^find/, function (next) {
-    this.find({ secretTour: { $ne: true } });
-    this.start= Date.now();
-    next();
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
 });
 tourSchema.post(/^find/, function (docs, next) {
-    console.log(`Query took ${Date.now() - this.start} milliseconds`);
-    console.log(docs);
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
+  next();
+});
+
+//AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
     next();
-})
+});
 const Tour = mongoose.model('Tour', tourSchema);
 
-module.exports = Tour
+module.exports = Tour;
