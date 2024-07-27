6e34301c7669b18cb2144368fb466a042a8b953e
TalaatEysa <talaateysa@outlook.com>
2024-07-27 19:44:34 +0300

    handle unhandled routes

--- app.js
+++ app.js
@@ -13,17 +13,19 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-	console.log('hello from middleware');
-	next();
-});
-app.use((req, res, next) => {
-	req.requestTime = new Date().toISOString();
-	next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
 // Route Handlers
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
