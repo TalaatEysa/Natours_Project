5f624c127d90eea9ee12102ad4cb02a319689580
TalaatEysa <talaateysa@outlook.com>
2024-07-28 11:16:27 +0300

    catching errors in asyn functions

---
+++ utils/catchAsync.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line arrow-body-style
+// module.exports = (fn) => {
+//   return (req, res, next) => {
+//     fn(req, res, next).catch(next);
+//   };
+// };
+module.exports = (fn) => (req, res, next) => {
+  fn(req, res, next).catch(next);
+};
