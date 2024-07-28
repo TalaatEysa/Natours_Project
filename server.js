d45fca50a3d6c2dfa17c88e2953ceba381f1c9ab
TalaatEysa <talaateysa@outlook.com>
2024-07-28 13:24:45 +0300

    handling unhandled rejections

--- server.js
+++ server.js
@@ -4,18 +4,29 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
 // const DB=process.env.DATABASE_LOCAL
-mongoose.connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
-}).then(() => console.log('DB connection successful!'));
-
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
