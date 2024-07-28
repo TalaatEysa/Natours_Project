8fc7cac87cd46c732ca5dd0fb6b5ea87fe992a60
TalaatEysa <talaateysa@outlook.com>
2024-07-28 13:14:22 +0300

    handling mongoose validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,13 +5,18 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 const handleDuplicateFieldsDB = (err) => {
-    // const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-    const value = err.keyValue.name;
-    const message = `Duplicate field value: ${value}. Please use another value!`;
-    return new AppError(message, 400);
-}
+  // const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const value = err.keyValue.name;
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
 const sendErrorDev = (err, res) => {
-    console.log(err);
+  console.log(err);
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
@@ -43,11 +48,12 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-      let error = { ...err };
-      console.log(error.message);
-      if (err.name === 'CastError') error = handleCastErrorDB(error);
-    //   if (error.kind === 'ObjectId') error = handleCastErrorDB(error); 
-      if(err.code === 11000) error = handleDuplicateFieldsDB(error);
+    let error = { ...err };
+    console.log(error.message);
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+    //   if (error.kind === 'ObjectId') error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
     sendErrorProd(error, res);
   }
 };
