1a5b50b4670db6fe24361690fc31adcefc532c25
TalaatEysa <talaateysa@outlook.com>
2024-08-01 23:45:16 +0300

    implementing rate limiting via express-rate-limit package

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -7,10 +8,16 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-// Middlewares
+// Global Middlewares
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
@@ -24,11 +31,10 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.all('*', (req, res, next) => {
-
-	// const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-	// err.status = 'fail';
-	// err.statusCode = 404;
-	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 app.use(globalErrorHandler);
 
