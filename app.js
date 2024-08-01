80474cf76869d5318803cbd84041bfdf1b856bdf
TalaatEysa <talaateysa@outlook.com>
2024-08-02 00:11:21 +0300

    implementing Data sanitization using express-mongo-sanitize and xss-clean packages

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongosanitize = require('express-mongo-sanitize');
+const xss=require('xss-clean')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -26,6 +28,12 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongosanitize());
+// Data sanitization against XSS
+app.use(xss());
+
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
 
