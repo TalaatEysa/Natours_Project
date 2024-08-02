09540b3395dc77a6da5235963cd4492a19e19234
TalaatEysa <talaateysa@outlook.com>
2024-08-02 23:39:27 +0300

    modeling tour guides in tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -102,6 +102,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      }
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -117,7 +123,12 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
-
+//Whn embedding document
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 //QUERY MIDDLEWARE
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
