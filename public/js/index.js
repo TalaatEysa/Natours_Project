e19cad9000b43773c3bd96b5cf42579cce78d192
TalaatEysa <talaateysa@outlook.com>
2024-08-11 19:27:05 +0300

    logging out users

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,24 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox.js';
-import { login } from './login.js';
+import { login, logout } from './login.js';
 
 //Dom Elements
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form');
-
+const logOutBtn = document.querySelector('.nav__el--logout');
 
 if (mapBox) {
-    const locations = JSON.parse(mapBox.dataset.locations);
-    displayMap(locations);
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
 }
 
 if (loginForm) {
-    document.querySelector('.form').addEventListener('submit', (e) => {
-        e.preventDefault();
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
+  document.querySelector('.form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
     login(email, password);
   });
 }
+
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
